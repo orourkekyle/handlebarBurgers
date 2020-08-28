4ac57837ae87bd5032e0ae26b42849f43b6a946b
zerobit <orourke_kyle@icloud.com>
2020-08-28 17:05:16 -0400

    converted starter code to match 'burger' theme, connected to database, installed dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Patty Melt');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Chili Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Salmon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
